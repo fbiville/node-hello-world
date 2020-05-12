4f5c852fc9581f22944f6d852abc3d4d3a417a8a
Florent Biville <fbiville@pivotal.io>
2020-05-12 17:10:25 +0200

    Switch to express app

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+const express = require('express')
+const app = express()
+
+const port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+const ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening at http://${ip}:${port}`));
