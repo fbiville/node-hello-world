d452a8abd597463ba5034a18c02b5a78703435f6
Florent Biville <fbiville@pivotal.io>
2020-06-29 17:48:21 +0200

    Add some blah

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const app = express()
 const port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 const ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Blah World!'));
 app.listen(port, () => console.log(`Example app listening at http://${ip}:${port}`));
